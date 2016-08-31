c4cbd3af2d3880946c2cbab7902cb1b126cb67d3
xcy <gary@qegoo.cn>
2016-08-31 16:51:20 +0800

    提交代码

---
+++ src/main/java/cn/hello/main/Main.java
@@ -0,0 +1,31 @@
+package cn.hello.main;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 主程序入口.
+ *
+ * @author xuping
+ */
+@EnableCaching
+@EnableScheduling
+@Configuration
+@ComponentScan("cn.hello")
+@MapperScan("cn.hello.world.dao")
+@EnableAutoConfiguration
+//@ImportResource(locations = {"classpath:beans-security.xml"})
+public class Main extends WebMvcConfigurerAdapter {
+    public static void main(final String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: asus-p
+  Date: 2016/8/29
+  Time: 14:35
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+    hello world!
+</body>
+</html>

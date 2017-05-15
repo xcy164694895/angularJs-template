a676ed07d41fcfe2a0d5293a4f1a9bdbda994b96
xcy <gary@qegoo.cn>
2017-05-15 17:16:46 +0800

    提交angularjs模板

--- src/main/java/cn/hello/world/web/IndexController.java
+++ src/main/java/cn/hello/world/web/IndexController.java
@@ -1,14 +1,19 @@
 package cn.hello.world.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-        return "index";
+        return "/index.html";
     }
 
     @RequestMapping("/manage")
@@ -16,4 +21,15 @@ public class IndexController {
         return "base/manage";
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/api/login", method = RequestMethod.POST)
+    public User login(@RequestBody  Map<String,String> map){
+        String email = map.get("username");
+        String password = map.get("password");
+        User user = new User();
+        user.setEmail(email);
+        user.setUsername(email);
+        user.setPassword(password);
+        return user;
+    }
 }
